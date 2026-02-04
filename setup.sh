34d4c539af3fb8ce402a88d3e22dd7db3f3a8d5f
Marius-Vlad DUMITRU <marius_vlad.dumitru@stud.etti.upb.ro>
2026-02-05 00:19:33 +0200

    Added setup.sh, the setup script for the local git repository. Git clone and run setup.sh.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# setup script
+
+config-repo gitlab.cs.pub.ro
+git remote add overleaf.com https://git@git.overleaf.com/6983b1de9612d1f69faa22ed 
+git config credential.helper store
+echo "Don't forget to generate a new Overleaf Git authentication token or copy-paste, when required, the one that you already have."
+echo "Don't forget that you changed the primary email on your Overleaf.com account to the upb one. Don't forget to change it back after you defended your dissertation."
