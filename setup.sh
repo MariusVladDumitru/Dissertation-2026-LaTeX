f972fd8c03bc4593809568e12f1c6da3a38f40c5
Marius-Vlad DUMITRU <marius_vlad.dumitru@stud.etti.upb.ro>
2026-03-06 22:23:09 +0200

    Update. Modified .gitignore and chmod -R 700 all files except teza-upb.cls, settings.tex, Makefile.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # setup script
 
-config-repo gitlab.cs.pub.ro
+config-repo.sh gitlab.cs.pub.ro
 git remote add overleaf.com https://git@git.overleaf.com/6983b1de9612d1f69faa22ed 
 git config credential.helper store
 echo "Don't forget to generate a new Overleaf Git authentication token or copy-paste, when required, the one that you already have."
